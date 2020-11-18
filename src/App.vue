3ca80de1d757657ec5f832d345bf078b1f95cd42
davidchen <david828107073@gmail.com>
2020-11-18 19:44:59 +0800

    new api

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  created() {
+    const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
+    //API 伺服器路徑
+    // 所申請的 API Path
+    console.log(api);
+    this.$http
+      .get(api)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
